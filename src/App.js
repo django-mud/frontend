daa9bc602d55864e0428ea55a75d15c7048b376e
zeleke <zzdd9251@gmail.com>
2020-03-03 20:59:23 -0800

    framwork .. commit..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
+import UserProvider from './contexts/UserContext';
+import DataProvider from './contexts/DataContext';
+
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import Register from './components/Register';
+import Game from './components/Game';
+import PrivateRoute from './components/PrivateRoute';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<UserProvider>
+				<Nav />
+				<Route exact path='/' component={Home} />
+				<Route path='/about' component={About} />
+				<Route path='/login' component={Login} />
+				<Route path='/register' component={Register} />
+				<DataProvider>
+					<PrivateRoute path='/game' component={Game} />
+				</DataProvider>
+			</UserProvider>
+		</div>
+	);
 }
 
 export default App;
